f3740d05e9b10885514767291f504bc9e0567fb9
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-03-03 16:58:57 +0530

    Changed component

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -1,56 +1,67 @@
-import './section1.css';
+import "./section1.css";
 
 const Section1 = () => {
-    return (
-      <>
-        <section className="section1">
-          <div className="img__container">
+  return (
+    <div className="white__container">
+      <div className="section1__container">
+        <div className="section1__left">
+          <div className="left__img">
             <img
+              className="section1__img"
               src="https://images.unsplash.com/photo-1519778119555-47f8a0929968?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-              alt=""
+              alt="img"
             />
           </div>
-          <div className="right__container">
-            <div className="text__container">
-              <h2>We Are The Leader in The Architecture</h2>
-              <p>
-                For each project we establish relationships with partners who we
-                know will help us create added value for your project. As well
-                as bringing together the public and private sectors, we make
-                sector-overarching links to gather knowledge and to learn from
-                each other.
-              </p>
-              <div className="section1__info">
-                <div className="section1__info__item">
-                  <h5>850</h5>
-                  <p>
-                    Properties
-                    <br />
-                    Built
-                  </p>
-                </div>
-                <div className="section1__info__item">
-                  <h5>24</h5>
-                  <p>
-                    Awards
-                    <br />
-                    Won
-                  </p>
-                </div>
-                <div className="section1__info__item">
-                  <h5>12</h5>
-                  <p>
-                    Years
-                    <br />
-                    Experience
-                  </p>
-                </div>
+        </div>
+        <div className="section1__right">
+          <div className="right__text__container">
+            <h2 className="section1__head">
+              We Are The Leader
+              <br /> in The Architecture
+            </h2>
+            <p className="section1__text">
+              For each project we establish relationships with partners who we
+              know
+              <br />
+              will help us create added value for your project. As well as
+              bringing
+              <br />
+              together the public and private sectors, we make
+              sector-overarching
+              <br />
+              links to gather knowledge and to learn from each other.
+            </p>
+            <div className="section1__info">
+              <div className="info__item">
+                <h2 className="lgText">850</h2>
+                <span className="smText">
+                  Property
+                  <br />
+                  Build
+                </span>
+              </div>
+              <div className="info__item">
+                <h2 className="lgText">24</h2>
+                <span className="smText">
+                  Awards
+                  <br />
+                  Gained
+                </span>
+              </div>
+              <div className="info__item">
+                <h2 className="lgText">15</h2>
+                <span className="smText">
+                  Years
+                  <br />
+                  Experience
+                </span>
               </div>
             </div>
           </div>
-        </section>
-      </>
-    );
-}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Section1;
